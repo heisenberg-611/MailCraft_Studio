843dd8247e2c5327b0b0c44fe155669713a821df
heisenberg-611 <lnsaha1214@gmail.com>
2026-09-06 03:24:36 +0600

    chore: reset application version number to 1.0

--- js/preset-manager.js
+++ js/preset-manager.js
@@ -121,7 +121,7 @@ const PresetManager = {
     const presets = this.getUserPresets();
     const payload = {
       app: 'MailCraft Studio',
-      version: '2.4.0',
+      version: '1.0.0',
       exportedAt: new Date().toISOString(),
       presets
     };
